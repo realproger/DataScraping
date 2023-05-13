9ea92b41f10704f829a8de16a6cd6de404d817ee
superuser <superuser@realproger.local>
2023-05-13 23:47:59 +0500

    Extracting data

--- my_data_scraper.py
+++ my_data_scraper.py
@@ -13,3 +13,10 @@ import csv
 def request_github_trending(url):
     res = requests.get(url)
     return res.text
+
+# Part 1: Extract
+def extract(page):
+    soup = BeautifulSoup(page, 'html.parser')
+    repos_list = soup.find_all('article', {'class' : 'Box-row'})
+
+    return repos_list
